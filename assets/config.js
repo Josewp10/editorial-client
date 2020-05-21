f39bf6b8b6fabb5a1c942a33d95ca01039c398c4
Josewp10 <jdmowp10@gmail.com>
2020-05-21 09:55:20 -0500

    Cambios

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,8 @@
-let url = "http://127.0.0.1:8000/";
+let url = "http://127.0.0.1:8000/api/v1";
 if (process.env.NODE_ENV == "production") {
-  url = "https://practica-editorial-api.herokuapp.com/";
+  url = "https://practica-editorial-api.herokuapp.com/api/v1";
 } else {
-  url = "http://127.0.0.1:8000/";
+  url = "http://127.0.0.1:8000/api/v1";
 }
 
 const config = { url_api: url };
